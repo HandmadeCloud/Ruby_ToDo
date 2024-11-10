6cd810e1e1cbc104ce949706476a0e78d21083de
JoJaeHyeon <joje1003@naver.com>
2024-11-10 21:09:21 +0900

    Refactor : :recycle: navigation 수정 (삭제 예정 : 이유 React 도입)

--- app/javascript/controllers/header.js
+++ app/javascript/controllers/header.js
@@ -1,14 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const dropdownTrigger = document.querySelector('.nav_etc .nav_tit');
-    const dropdownMenu = document.querySelector('.nav_etc .open_list');
+    const dropdownTrigger = document.querySelector('.nav_etc .box .box .nav_tit.language');
+    const dropdownMenu = document.querySelector('.nav_etc .box .box .open_list');
 
-    dropdownTrigger.addEventListener('click', () => {
-        dropdownMenu.classList.toggle('show'); // 'show' 클래스로 드롭다운 메뉴 표시
+    // 마우스가 언어 설정 버튼에 올라가면 드롭다운 메뉴 표시
+    dropdownTrigger.addEventListener('mouseover', () => {
+        dropdownMenu.style.display = 'block';
+        dropdownMenu.style.opacity = 1;
+        dropdownMenu.style.visibility = 'visible';
     });
 
-    document.addEventListener('click', (event) => {
-        if (!dropdownTrigger.contains(event.target) && !dropdownMenu.contains(event.target)) {
-            dropdownMenu.classList.remove('show');
-        }
+    // 마우스가 드롭다운 메뉴 영역에서 벗어나면 메뉴 숨기기
+    dropdownTrigger.addEventListener('mouseout', (event) => {
+        dropdownMenu.style.display = 'none';
+        dropdownMenu.style.opacity = 0;
+        dropdownMenu.style.visibility = 'visible';
+    });
+
+    // 드롭다운 메뉴에서 마우스가 나갈 때 닫히도록 설정
+    dropdownMenu.addEventListener('mouseout', (event) => {
+        dropdownMenu.style.display = 'none';
+        dropdownMenu.style.opacity = 0;
+        dropdownMenu.style.visibility = 'visible';
     });
 });
